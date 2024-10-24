d98a4a16790650aa26cc9f812a52c4394d8f531f
Med <geek.4tech007@gmail.com>
2024-10-24 14:45:19 +0000

    Added food model ✨

---
+++ lib/models/food_model.dart
@@ -0,0 +1,32 @@
+class Food {
+  final String name;
+  final String description;
+  final String image;
+  final String price;
+  final FoodCategory category;
+  final List<Addoon> addons;
+
+  const Food({
+    required this.category,
+    required this.addons,
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price,
+  });
+}
+
+enum FoodCategory {
+  burgers,
+  salads,
+  sides,
+  desserts,
+  drinks,
+}
+
+class Addoon {
+  final String name;
+  final double price;
+
+  Addoon({required this.name, required this.price});
+}
